0f9b871fa16ba0ecad65f37e27200edd58594abf
Ceci Ivanov <61785341+cceeci99@users.noreply.github.com>
2023-02-21 16:06:45 +0200

    Update kubecost.py

--- kubecost.py
+++ kubecost.py
@@ -10,7 +10,7 @@ import datetime as dt
 current_date = dt.datetime.now().strftime("%d_%m_%Y-%H:%M:%S")
 
 # KubeCost API url
-url = "http://20.4.129.247:9090/model/aggregatedCostModel"
+url = "http://IP:PORT/model/aggregatedCostModel"
 
 # Use the requests library to make a GET request to the KubeCost API
 params = (
@@ -62,7 +62,7 @@ df.to_csv(local_csv_file, index=False)
 ## TODO: store the csv somewhere (storageAccount, sharePoint) ##
 
 # parameter
-# CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=sqldbtest1312;AccountKey=RM907yZBkAcke+chv3xK8Fcsl1Qyj6J8TFPcXyIdmO8kvzz0WSVtUTvdp9RNPakX/gLEpuRBE7qy+AStXkR9DQ==;EndpointSuffix=core.windows.net"
+# CONNECTION_STRING = "***"
 # # parameter
 # CONTAINERNAME = "kubecost"
 
